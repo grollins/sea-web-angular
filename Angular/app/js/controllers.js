3ff7cddff70936e9265e28cb7acdfb4b5e1bab69
Geoff Rollins <grollins@gmail.com>
2013-11-30 16:15:06 -0800

    Implement table to display detailed results for jobs.

--- Angular/app/js/controllers.js
+++ Angular/app/js/controllers.js
@@ -91,13 +91,18 @@ var controllersModule = angular.module('angularProject.controllers', [])
 
     .controller('jobCtrl', function($scope, $http, $location, $routeParams, User) {
         $scope.jobId = $routeParams.jobId;
+        $scope.jobDone = false;
 
         $scope.refreshJob = function() {
             $http({method: 'GET', url: "http://localhost:8001/jobs/" + $scope.jobId + "/"}).
             success(function(data, status, headers, config) {
                 $scope.status = status;
                 $scope.job_data = data;
+                $scope.result = data.result[0];
                 console.log("Job refresh success");
+                if (data.status === 'Done') {
+                    $scope.jobDone = true;
+                }
             }).
             error(function(data, status) {
                 $scope.job_data = data || "Job refresh failed";
