2faa30d3f528a61d825491674e135ec12fc13c57
Geoff Rollins <grollins@gmail.com>
2013-12-20 09:27:53 -0800

    Remove backend from project.

--- Django/jobs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Job
-
-
-class JobAdmin(admin.ModelAdmin):
-    pass
-
-''' Register Admin layouts into django'''
-admin.site.register(Job, JobAdmin)
