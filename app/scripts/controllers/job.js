a9e53e21e14ebbd232d9ad3ec806c14059bb01c0
Geoff Rollins <grollins@gmail.com>
2013-12-20 13:09:06 -0800

    Implement job deletion button.

--- app/scripts/controllers/job.js
+++ app/scripts/controllers/job.js
@@ -1,25 +1,36 @@
 'use strict';
 
 angular.module('seaWebApp')
-    .controller('JobCtrl', function($scope, $http, $location, $routeParams, User) {
+    .controller('JobCtrl', function($scope, $http, $location, $routeParams) {
         $scope.jobId = $routeParams.jobId;
         $scope.jobDone = false;
 
         $scope.refreshJob = function() {
-            $http({method: 'GET', url: "http://localhost:8001/jobs/" + $scope.jobId + "/"}).
-            success(function(data, status, headers, config) {
+            $http({method: 'GET', url: 'http://localhost:8001/jobs/' + $scope.jobId + '/'}).
+            success(function(data, status) {
                 $scope.status = status;
-                $scope.job_data = data;
+                $scope.jobData = data;
                 $scope.result = data.result[0];
-                console.log("Job refresh success");
+                console.log('Job refresh success');
                 if (data.status === 'Done') {
                     $scope.jobDone = true;
                 }
             }).
             error(function(data, status) {
-                $scope.job_data = data || "Job refresh failed";
+                $scope.jobData = data || 'Job refresh failed';
                 $scope.status = status;
-                console.log("Job refresh failed");
+                console.log('Job refresh failed');
+            });
+        };
+
+        $scope.deleteJob = function() {
+            $http({method: 'DELETE', url:'http://localhost:8001/jobs/' + $scope.jobId + '/'}).
+            success(function() {
+                console.log('Job deleted');
+                $location.path( "/home" );
+            }).
+            error(function() {
+                console.log('Job deletion failed');
             });
         };
 
